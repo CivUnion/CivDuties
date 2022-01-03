2e86d0bf171344fd41df768ab088ec3283af9854
Cola <vaultslave@gmail.com>
2022-01-03 22:56:39 +0000

    CivGradle / 1.18.1

---
+++ paper/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+	`java-library`
+	id("net.civmc.civgradle.plugin")
+	id("io.papermc.paperweight.userdev") version "1.3.1"
+	id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
+civGradle {
+	paper {
+		pluginName = "CivDuties"
+	}
+}
+
+dependencies {
+	paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+
+    compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("net.civmc.combattagplus:paper:2.0.0-SNAPSHOT:dev")
+}
