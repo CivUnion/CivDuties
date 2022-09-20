71db867771c0b1c28eca2d9c867498ce13d2eba1
Aleksey Terzi <aleksey.terzi@gmail.com>
2022-09-19 18:45:33 -0600

    Remove CivGradle

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,20 +1,12 @@
 plugins {
 	`java-library`
-	id("net.civmc.civgradle.plugin")
-	id("io.papermc.paperweight.userdev") version "1.3.1"
-	id("com.github.johnrengelman.shadow") version "7.1.0"
-}
-
-civGradle {
-	paper {
-		pluginName = "CivDuties"
-	}
+	id("io.papermc.paperweight.userdev") version "1.3.8"
 }
 
 dependencies {
 	paperDevBundle("1.18.2-R0.1-SNAPSHOT")
 
-    compileOnly("net.civmc.civmodcore:paper:2.0.0-SNAPSHOT:dev-all")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
-    compileOnly("net.civmc.combattagplus:paper:2.0.0-SNAPSHOT:dev")
+	compileOnly("net.civmc.civmodcore:CivModCore:2.4.0:dev-all")
+    compileOnly("net.civmc.combattagplus:CombatTagPlus:2.1.0:dev")
 }
